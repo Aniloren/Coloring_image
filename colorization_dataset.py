294b598c88fc53b7d1d229529de734e74da7f30f
Aniloren <80814419+Aniloren@users.noreply.github.com>
2021-06-11 22:32:05 +0300

    Add colorization_dataset.py

---
+++ colorization_dataset.py
@@ -0,0 +1,36 @@
+import torch.utils.data as data
+
+from PIL import Image
+from torch.utils.data import Dataset
+from torchvision import transforms
+from skimage.color import rgb2lab, lab2rgb
+import numpy as np
+SIZE = 256 
+class ColorizationDataset(Dataset):
+    def __init__(self, paths, split='train'):
+        if split == 'train':
+            self.transforms = transforms.Compose([
+                transforms.Resize((SIZE, SIZE),  Image.BICUBIC),
+                transforms.RandomHorizontalFlip(), # A little data augmentation!
+            ])
+        elif split == 'val':
+            self.transforms = transforms.Resize((SIZE, SIZE),  Image.BICUBIC)
+        
+        self.split = split
+        self.size = SIZE
+        self.paths = paths
+    
+    def __getitem__(self, idx):
+        img = Image.open(self.paths[idx]).convert("RGB")
+        img = self.transforms(img)
+        img = np.array(img)
+        img_lab = rgb2lab(img).astype("float32") # Converting RGB to L*a*b
+        img_lab = transforms.ToTensor()(img_lab)
+        L = img_lab[[0], ...] / 50. - 1. # Between -1 and 1
+        ab = img_lab[[1, 2], ...] / 110. # Between -1 and 1
+        
+        return {'L': L, 'ab': ab}
+    
+    def __len__(self):
+        return len(self.paths)
+
